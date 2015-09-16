996a9e1b09c0077ded9e0ac5e7df21a9ba236e39
Fernando Henrique Crozetta <fernando.crozetta@outlook.com>
2015-09-16 20:16:26 -0300

    Creation of: Board class; Configuration package;

---
+++ Python-text-based-game-engine/Configuration/Board.py
@@ -0,0 +1,7 @@
+﻿import numpy
+
+class Board(object):
+    """Creates a Matrix board"""
+    def __init__(self, height,width):
+        self.board = numpy.empty(shape=(height,width))
+
---
+++ Python-text-based-game-engine/Python_text_based_game_engine.py
@@ -0,0 +1,7 @@
+import sys
+import os
+from Configuration import Board
+
+matrix = Board.Board(10,5)
+
+print(matrix.board)
