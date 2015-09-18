c292383936e02184cfcc9342b7d35811741bf275
Fernando Henrique Crozetta <fernando.crozetta@outlook.com>
2015-09-17 21:05:54 -0300

    Implemented partially Item class

---
+++ Python-text-based-game-engine/BoardGame/Item.py
@@ -0,0 +1,18 @@
+﻿from enum import Enum
+
+class Attributes(Enum):
+    attack=0
+    defense=1
+    health=2
+    mana=3
+
+class Item(object):
+    """Creates Items"""
+    def __init__(self, id,title,description='No description',weight=1):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.weight = weight
+        self.attributes = {}
+
+
--- Python-text-based-game-engine/Python_text_based_game_engine.py
+++ Python-text-based-game-engine/Python_text_based_game_engine.py
@@ -1,8 +1,11 @@
 ﻿import sys
 import os
-from BoardGame import Board, Character
+from BoardGame import Board, Character,Item
 
 def main():
+    sword = Item.Item('wooden sword','wooden flying sword of flying pony from hell','this shit is amazing',30)
+    sword.attributes[Item.Attributes.defense] = 12
+    print (sword.attributes)
     pass
 
 if __name__ == "__main__":
